07f2a5614125ac7bfaf674cdc789b550214862e7
Daniel <feifei282922334@gmail.com>
2020-04-12 01:40:01 +0800

    新增区块 添加到数据库中

---
+++ part16-persistence-addblock/BLC/Block.go
@@ -0,0 +1,95 @@
+package BLC
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"log"
+	"time"
+)
+
+//定义区块
+type Block struct {
+	//1.区块高度
+	Height int64
+	//2.上一个区块得hash
+	PrevBlockHash []byte
+	//3.交易数据
+	Data []byte
+	//4.时间戳
+	Timestamp int64
+	//5.hash
+	Hash []byte
+	//6.Nonce
+	Nonce int64
+}
+
+//func (block *Block) SetHash() {
+//	//1.height转换为字节数组[]byte
+//	heightBytes := IntToHed(block.Height)
+//
+//	//2.时间戳转换为字节数组[]byte
+//	timeString := strconv.FormatInt(block.Timestamp, 2)
+//	timeBytes := []byte(timeString)
+//	//3.拼接所有得属性
+//	blockBytes := bytes.Join([][]byte{heightBytes, block.PrevBlockHash, block.Data, timeBytes, block.Hash}, []byte{})
+//	//4.生成Hash
+//	//将二维的切片数组连接起来返回一个一维的切片
+//	hash := sha256.Sum256(blockBytes)
+//	//sha256 返回得是一个32字节得数组 需要转换一下
+//	block.Hash = hash[:]
+//}
+//将区块对象序列化 成字节数组
+func (block *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(block)
+	if err != nil {
+		log.Panic(err)
+	}
+	return result.Bytes()
+}
+
+//反序列化
+func DeserializeBlock(blockBytes []byte) *Block {
+	var block Block
+	decoder := gob.NewDecoder(bytes.NewReader(blockBytes))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+	return &block
+}
+
+//1.创建新得区块
+func NewBlock(data string, height int64, prevBlockHash []byte) *Block {
+
+	//创建区块
+	block := &Block{
+		Height:        height,
+		PrevBlockHash: prevBlockHash,
+		Data:          []byte(data),
+		Timestamp:     time.Now().Unix(),
+		Hash:          nil,
+		Nonce:         0,
+	}
+
+	//设置Hash
+	//block.SetHash()
+	//调用工作量证明得方法并且返回有效得Hash和Nonce值
+	pow := NewProofOfWork(block)
+
+	//挖矿验证
+	hash, nonce := pow.Run()
+	block.Hash = hash
+	block.Nonce = nonce
+	fmt.Println()
+	return block
+}
+
+//2.单独写一个方法 生成创世区块
+func CreateGenesisBlock(data string) *Block {
+
+	return NewBlock(data, 1, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+}
---
+++ part16-persistence-addblock/BLC/Blockchain.go
@@ -0,0 +1,101 @@
+package BLC
+
+import (
+	"github.com/boltdb/bolt"
+	"log"
+)
+
+const dbName = "blockChain.db"
+const blockTableName = "blocks"
+
+type BlockChain struct {
+	//Blocks []*Block //存储有序得区块
+	Tip []byte //最新得区块得Hash
+	DB  *bolt.DB
+}
+
+//2.增加区块到区块链里面
+func (blc *BlockChain) AddBlockToBlockChain(data string) {
+
+	//往链里面添加区块
+	//blc.Blocks = append(blc.Blocks, newBlock)
+	err := blc.DB.Update(func(tx *bolt.Tx) error {
+		//1.获取表
+		b := tx.Bucket([]byte(blockTableName))
+		//2.创建新区块
+		if b != nil {
+
+			//先获取最新区块
+			blockBytes := b.Get(blc.Tip)
+			//反序列化
+			block := DeserializeBlock(blockBytes)
+
+			//3.将区块序列化并且存储到数据库中
+			newBlock := NewBlock(data, block.Height+1, block.Hash)
+			err := b.Put(newBlock.Hash, newBlock.Serialize())
+			if err != nil {
+				log.Panic(err)
+			}
+			//4.更新数据库里面"l"对应得hash
+			err = b.Put([]byte("l"), newBlock.Hash)
+			if err != nil {
+				log.Panic(err)
+			}
+			//5.更新blockChain的Tip
+			blc.Tip = newBlock.Hash
+		}
+
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+//1.创建带有创世区块得区块链
+func CreateBlockChainWithGenesisBlock() *BlockChain {
+
+	//创建或打开数据库
+	db, err := bolt.Open(dbName, 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var blockHash []byte
+
+	err = db.Update(func(tx *bolt.Tx) error {
+
+		//获取表
+		b := tx.Bucket([]byte(blockTableName))
+
+		if b == nil {
+			//创建数据库表
+			b, err = tx.CreateBucket([]byte(blockTableName))
+			if err != nil {
+				log.Panic(err)
+			}
+		}
+
+		if b != nil {
+			//创建创世区块
+			genesisBlock := CreateGenesisBlock("Genesis Data.....")
+			//将创世区块存储到表当中
+			err := b.Put(genesisBlock.Hash, genesisBlock.Serialize())
+			if err != nil {
+				log.Panic(err)
+			}
+			//存储最新区块得Hash
+			err = b.Put([]byte("l"), genesisBlock.Hash)
+			if err != nil {
+				log.Panic(err)
+			}
+			blockHash = genesisBlock.Hash
+		}
+		return nil
+	})
+	//defer db.Close()
+
+	//返回区块链对象
+	//return &BlockChain{Blocks: []*Block{genesisBlock}}
+	return &BlockChain{blockHash, db}
+}
