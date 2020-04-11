65cd99426905d291d76ba2f414ad5b26dbbd2f93
Daniel <feifei282922334@gmail.com>
2020-04-12 00:14:25 +0800

    bolt数据库

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module publicChain
 
 go 1.14
+
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	golang.org/x/sys v0.0.0-20200408040146-ea54a3c99b9b // indirect
+)
---
+++ part9-Serialize-DeserializaBlock/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"publicChain/part9-Serialize-DeserializaBlock/BLC"
+)
+
+func main() {
+	//创世区块
+	//blockChain := BLC.CreateBlockChainWithGenesisBlock()
+
+	////添加新区块
+	//blockChain.AddBlockToBlockChain("Send 100RMB To A", blockChain.Blocks[len(blockChain.Blocks)-1].Height+1, blockChain.Blocks[len(blockChain.Blocks)-1].Hash)
+	//blockChain.AddBlockToBlockChain("Send 200RMB To B", blockChain.Blocks[len(blockChain.Blocks)-1].Height+1, blockChain.Blocks[len(blockChain.Blocks)-1].Hash)
+	//blockChain.AddBlockToBlockChain("Send 300RMB To C", blockChain.Blocks[len(blockChain.Blocks)-1].Height+1, blockChain.Blocks[len(blockChain.Blocks)-1].Hash)
+	//blockChain.AddBlockToBlockChain("Send 400RMB To D", blockChain.Blocks[len(blockChain.Blocks)-1].Height+1, blockChain.Blocks[len(blockChain.Blocks)-1].Hash)
+	//blockChain.AddBlockToBlockChain("Send 500RMB To E", blockChain.Blocks[len(blockChain.Blocks)-1].Height+1, blockChain.Blocks[len(blockChain.Blocks)-1].Hash)
+	//
+	//fmt.Println(blockChain)
+	//fmt.Println(blockChain.Blocks)
+	block := BLC.NewBlock("Test", 1, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+	fmt.Printf("%d\n", block.Nonce)
+	fmt.Printf("%x\n", block.Hash)
+	proofOfwork := BLC.NewProofOfWork(block)
+	fmt.Printf("%v", proofOfwork.IsValid())
+}
