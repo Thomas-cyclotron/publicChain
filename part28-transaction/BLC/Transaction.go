b4c0fa424782a41095485ba4446e4ec672b05a4a
Daniel <feifei282922334@gmail.com>
2020-04-12 16:38:47 +0800

    初次构建 Transaction

---
+++ part28-transaction/BLC/Transaction.go
@@ -0,0 +1,11 @@
+package BLC
+
+// UTXO
+type Transaction struct {
+	//1. 交易hash
+	TxHash []byte
+
+	//2. 输入
+
+	//3. 输出
+}
---
+++ part28-transaction/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"publicChain/part28-transaction/BLC"
+)
+
+func main() {
+
+	cli := BLC.CLI{}
+
+	cli.Run()
+
+}
